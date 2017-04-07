c9941ef93a0ea267f7de9a8fddffbb0f27703694
Danilo <Danilo@TIMON>
2017-04-07 15:10:39 +0200

    Request get

---
+++ src/main/java/com/udemy/controller/Example2controlller.java
@@ -0,0 +1,34 @@
+package com.udemy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.thymeleaf.standard.processor.text.StandardTextInliningTextProcessor;
+
+@Controller
+@RequestMapping("/Example2")
+public class Example2controlller {
+
+	private static final String EXAMPLE2_VIEW = "example2";
+	// http://localhost:8080/Example2/request1?nm=carolina
+	@GetMapping("/request1")
+	public ModelAndView request1(@RequestParam(name = "nm", required = false, defaultValue = "null") String name) {
+		ModelAndView mav = new ModelAndView(EXAMPLE2_VIEW);
+		mav.addObject("nm_in_model", name);
+		return mav;
+
+	}
+	// http://localhost:8080/Example2/request1/carolina
+	@RequestMapping("/request2/{nm}")
+	public ModelAndView request2(@PathVariable("nm")String name ){
+		ModelAndView mav = new ModelAndView(EXAMPLE2_VIEW);
+		mav.addObject("nm_in_model", name);
+		return mav;
+		
+	}
+	
+
+}
--- src/main/java/com/udemy/controller/ExampleController.java
+++ src/main/java/com/udemy/controller/ExampleController.java
@@ -1,5 +1,11 @@
 package com.udemy.controller;
 
+import static org.assertj.core.api.Assertions.setAllowComparingPrivateFields;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.omg.CORBA.PRIVATE_MEMBER;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,12 +18,12 @@ import com.udemy.model.Person;
 @RequestMapping("/example")
 public class ExampleController {
 
-	public static final String EXAMPLE_VIEW = "example";
+	public static final String EXAMPLE_VIEW = "people";
 
 	// PRIMERA FORMA REDIRECCIONES
 	@GetMapping("/exampleString")
 	public String exampleString(Model model) {
-		model.addAttribute("person", new Person("jon", 24));
+		model.addAttribute("people",getPeople());
 		return EXAMPLE_VIEW;
 
 	}
@@ -26,9 +32,20 @@ public class ExampleController {
 	@GetMapping("/exampleMAV")
 	public ModelAndView exampleMAV() {
 		ModelAndView name = new ModelAndView(EXAMPLE_VIEW);
-		name.addObject("person", new Person("mikel", 29));
-		// return new ModelAndView(EXAMPLE_VIEW);
+		name.addObject("people", getPeople());
+		
 		return name;
 
 	}
+
+	private List<Person> getPeople() {
+	      List<Person> people = new ArrayList<>();
+	        people.add(new Person("Carlos",20));
+	        people.add(new Person("juan",50));
+	        people.add(new Person("Carlos",20));
+	        people.add(new Person("juan",50));
+	        return people;
+	        
+}
+
 }
