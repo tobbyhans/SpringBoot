8ecadd3bb7221b249fcda99d676cad3217072f57
Danilo <Danilo@TIMON>
2017-04-22 13:17:25 +0200

    // # REDIRECT FORMA 2 ESTO ESPERA COMO UNA URL

--- src/main/java/com/udemy/controller/Example3Contoller.java
+++ src/main/java/com/udemy/controller/Example3Contoller.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 import com.udemy.model.Person;
 
@@ -16,13 +17,19 @@ public class Example3Contoller {
 
 	public static final String FORM_VIEW = "form";
 	public static final String RESUL_VIEW = "result";
-	
-	//# REDIRECT FORMA 1
+
+//	// # REDIRECT FORMA 1
+//	 @GetMapping("/")
+//	 public String redirect(){
+//	return "redirect:/example3/showform";	 
+//	 }
+
+	// # REDIRECT FORMA 2 ESTO ESPERA COMO UNA URL
+
 	@GetMapping("/")
-	public String redirect(){
-		return "redirect:/example3/showform";
+	public RedirectView redirect() {
+		return new RedirectView("http:/example3/showform");
 	}
-	
 
 	@GetMapping("/showform")
 	public String showForm(Model model) {
@@ -32,11 +39,10 @@ public class Example3Contoller {
 	}
 
 	@PostMapping("/addperson")
-		public ModelAndView addPerson(@ModelAttribute("person")Person person){
+	public ModelAndView addPerson(@ModelAttribute("person") Person person) {
 		ModelAndView mav = new ModelAndView(RESUL_VIEW);
 		mav.addObject("person", person);
 		return mav;
 	}
-	
-	
+
 }
