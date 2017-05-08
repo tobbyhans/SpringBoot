8ee3606287b4ed9fb441e38b83889db3306074a6
Danilo <Danilo@TIMON>
2017-05-08 19:50:38 +0200

    #/ Add query wich jpaRepository

--- src/main/java/com/udemy/component/ExampleComponent.java
+++ src/main/java/com/udemy/component/ExampleComponent.java
@@ -2,14 +2,26 @@ package com.udemy.component;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-@Component("ExampleComponent")
+import com.udemy.repository.CourseJpaRepository;
+
+@Component("exampleComponent")
 public class ExampleComponent {
 	
 	private static final Log LOG =LogFactory.getLog(ExampleComponent.class);
 
+	
+	@Autowired
+	@Qualifier("courseJpaRepository")
+	private CourseJpaRepository courseJpaRepository;
+	
 	public void sayhello(){
+		
+		courseJpaRepository.count();
+		
 		LOG.info("example for taza COMPONET.....");
 		
 	}
--- src/main/java/com/udemy/repository/CourseJpaRepository.java
+++ src/main/java/com/udemy/repository/CourseJpaRepository.java
@@ -1,13 +1,21 @@
  package com.udemy.repository;
 
  import java.io.Serializable;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.udemy.entity.Course;
 
- @Repository("CourseJpaRepository")
+ @Repository("courseJpaRepository")
  public interface CourseJpaRepository extends JpaRepository<Course, Serializable>{
+	 
+	//Consultas con jpareposytory
+	 public abstract Course findByPrice(int price);
+	 
+	 public abstract Course findByNameAndprice(String name , int price);
+	 
+	 public abstract List<Course>  findByNameOrderByHours(String name);
 
  }
